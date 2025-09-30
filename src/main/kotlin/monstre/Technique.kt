041dd53efa841dddfacb254a44f911cff7c39224
FilloyLuca <filloylucapro@gmail.com>
2025-09-30 10:46:46 +0200

    fun calculerPrecision() fait

--- src/main/kotlin/monstre/Technique.kt
+++ src/main/kotlin/monstre/Technique.kt
@@ -1,5 +1,7 @@
 package org.example.monstre
 
+import kotlin.random.Random
+
 class Technique(
     val id : Int,
     val nom : String,
@@ -10,5 +12,15 @@ class Technique(
     val faireDegat : Boolean,
     val elementTechnique : Element
 ) {
-
+    /**
+     * Calcule si une technique touche sa cible en fonction de sa précision.
+     *
+     * @param technique La technique utilisée, contenant un attribut `precision` (valeur entre 1 et 100).
+     * @return true si la technique touche (aléatoire ≤ précision), false sinon.
+     */
+    fun calculerPrecision(technique : Technique) : Boolean{
+        val nb = Random.nextInt(1, 101)  // 101 est exclusif, donc génère de 1 à 100
+        if (nb <= technique.precision) return true
+        else return false
+    }
 }
